c939b9400e24a5ce8e0685edadee26c584f0e47c
Efraín Gourcy Lara <106822741+egourcy@users.noreply.github.com>
2023-09-08 22:51:21 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # replaceBlanksAccents
 Replace blank spaces with "-" character &amp; remove accents from file name
+
+Built in Python 3.11.3:
+
+## Requirements
+
+- Python 3.x
+
+## Example
+
+### Windows
+
+py.exe removeBlanks.py c:\path\to\folder
+
+### GNU/Linux
+
+python3 removeBlanks.py /path/to/folder
+
+#### Result
+
+file-name-out.txt
---
+++ removeBlanks.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+# Replace blank spaces with "-" character & remove accents
+def split(path):
+  folder = os.listdir(path)
+  counter = 0
+  for files in folder:
+    blank = files.find(' ')
+    if (blank != -1):
+      replace = files.replace(' ', '-')
+      replace = replace.replace('á', 'a')
+      replace = replace.replace('é', 'e')
+      replace = replace.replace('é', 'i')
+      replace = replace.replace('ó', 'o')
+      replace = replace.replace('ú', 'u')
+      replace = replace.replace('ñ', 'n')
+      os.rename(path+"\\"+files, path+"\\"+replace)
+      print("\n", replace)
+      counter += 1
+  
+  print("\nSe modificaron ", counter , " archivos\n")
+
+# Select path
+def setDiectory(directory):
+  try:
+    if (directory):
+     split(directory)
+  except:
+    question = input("No paso argumentos o el directorio es incorrecto, ¿Desea continuar con el directorio actual? (S/N) ")
+    if (question == 'S' or question == 's' or question == 'SI' or question == 'Si' or question == 'si'):
+      split(os.getcwd())
+#
+# __main__
+#
+if __name__ == '__main__':
+  setDiectory(sys.argv[1])
